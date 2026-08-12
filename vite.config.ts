0f0d27c02e0606d52974125faab3bc6012471b11
Poorvija06 <poorvija@gmail.com>
2026-08-13 04:25:08 +0530

    Fix production asset path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-   base:'/placemate-ai/',
+   base:'/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
