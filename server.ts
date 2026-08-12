4c7f2c1958b494422f3011af605da24ef15031c8
Poorvija06 <poorvija@gmail.com>
2026-08-13 04:05:03 +0530

    changed server.ts file

--- server.ts
+++ server.ts
@@ -1797,11 +1797,16 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(process.cwd(), 'dist');
-    app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next();
   }
+
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`PlaceMate AI Server running on http://0.0.0.0:${PORT}`);
